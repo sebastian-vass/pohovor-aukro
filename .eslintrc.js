fc4b37609e5c990e322bef80d0be9662ea0209fb
sebastianvass <sebastian.vass@peckadesign.cz>
2022-11-30 23:06:16 +0100

    Running eslint over files

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,10 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: './tsconfig.json'
   },
   rules: {
+    '@typescript-eslint/strict-boolean-expressions': 'off'
   }
 }
--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents (on, config) {
       config.baseUrl = 'https://aukro.cz'
       return config
     }
   }
-});
+})
