1e9b4c47f11f787746118347e8c4c6d3ee3b4846
sebastianvass <sebastian.vass@peckadesign.cz>
2022-12-17 16:20:07 +0100

    Change test name

---
+++ cypress/e2e/aukro.cy.ts
@@ -0,0 +1,5 @@
+describe('Aukro task assignment', () => {
+  it('', () => {
+    cy.visit('/')
+  })
+})
--- cypress/e2e/todo.cy.ts
+++
@@ -1,5 +0,0 @@
-describe('Test init', () => {
-  it('Homepage open', () => {
-    cy.visit('/')
-  })
-})
