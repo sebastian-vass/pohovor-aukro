fdbb04a2ce0f565149b8f64170720136666f8d16
sebastianvass <sebastian.vass@peckadesign.cz>
2022-12-17 22:14:47 +0100

    Manually closing GDPR and waiting on full load page

--- cypress/e2e/aukro.cy.ts
+++ cypress/e2e/aukro.cy.ts
@@ -1,5 +1,11 @@
 describe('Aukro task assignment', () => {
-  it('', () => {
+  before(() => {
+    cy.intercept('GET', '/sid/json?**').as('gdpr')
     cy.visit('/')
+    cy.get('#didomi-notice-agree-button').click()
+    cy.wait('@gdpr')
+  })
+
+  it('', () => {
   })
 })
