5316f1d61dbe12850b8e50df6433a40fd7f7476e
sebastianvass <sebastian.vass@peckadesign.cz>
2022-12-17 16:16:24 +0100

    Add new config rules

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,10 @@ export default defineConfig({
   e2e: {
     setupNodeEvents (on, config) {
       config.baseUrl = 'https://aukro.cz'
+      config.video = false
+      config.viewportHeight = 1080
+      config.viewportWidth = 1400
+
       return config
     }
   }
